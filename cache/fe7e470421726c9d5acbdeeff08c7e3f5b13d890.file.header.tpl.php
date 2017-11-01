7175f04cb20783ef6dd9f4c51dc1f96625c62cc5
achluky <luky.lucky24@gmail.com>
2017-11-01 23:02:43 +0700

    change TITLE HEADER

--- cache/fe7e470421726c9d5acbdeeff08c7e3f5b13d890.file.header.tpl.php
+++ cache/fe7e470421726c9d5acbdeeff08c7e3f5b13d890.file.header.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.21-dev, created on 2017-10-31 17:52:20
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2017-11-01 17:01:34
          compiled from "application/views/admin/header.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:213764879259f8aa4489a247-30813980%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'fe7e470421726c9d5acbdeeff08c7e3f5b13d890' => 
     array (
       0 => 'application/views/admin/header.tpl',
-      1 => 1509031609,
+      1 => 1509552090,
       2 => 'file',
     ),
   ),
@@ -15,12 +15,12 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
-  'has_nocache_code' => false,
   'version' => 'Smarty-3.1.21-dev',
   'unifunc' => 'content_59f8aa448a4e70_64524530',
+  'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_59f8aa448a4e70_64524530')) {function content_59f8aa448a4e70_64524530($_smarty_tpl) {?>    <meta charset="UTF-8">
-    <title>Belantara Fondation</title>
+    <title>CMS : BELANTARA FOUNDATION</title>
     <link rel="icon" type="image/png" href="<?php echo base_url();?>
 assets/img/ipb.ico" />
 	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
